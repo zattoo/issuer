eb5f6b3d587b5904794c813a2968b516d2dc3322
Nitzan Nashi <nitzan.nashi@zattoo.com>
2021-04-18 15:02:08 +0300

    build

--- dist/index.js
+++ dist/index.js
@@ -5954,18 +5954,26 @@ const run = async () => {
   const host = core.getInput('host', {required: true});
   const title = core.getInput('title', {required: false});
 
+  core.info('Running the Action');
+
   const octokit = getOctokit(token);
   const {pull_request} = context.payload;
 
   const ticketsDescription = utils.createTicketsDescription(host, pull_request.title, title);
   const updatedBody = utils.updateBody(pull_request.body, ticketsDescription);
 
+  core.debug({ticketsDescription});
+  core.debug({updatedBody});
+
   await octokit.pulls.update({
     owner: context.repo.owner,
     repo: context.repo.repo,
     pull_number: pull_request.number,
-    updatedBody,
+    body: updatedBody,
   });
+
+  core.info('Description updated successfully');
+
 };
 
 run();
