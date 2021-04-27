d491377fd3b3a79e630c2548a2e8d7f5f95a4ade
Nitzan Nashi <nitzan.nashi@zattoo.com>
2021-04-27 09:54:22 +0200

    Remove space

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,6 @@ const TITLE_DEFAULT_VALUE = '### Issuer';
 const TICKETS_BLOCK_START = '<!-- tickets start -->';
 const TICKETS_BLOCK_END = '<!-- tickets end -->';
 const VERIFY_DEFAULT_VALUE = false;
-
 const FORMAT_ERROR = 'Title format is invalid, expected pattern is CODE-XXX [, CODE-XXX]: Title';
 const REFERENCE_ERROR = 'Issuer reference is missing. Expected pattern is CODE-XXX [, CODE-XXX]: Title';
 
