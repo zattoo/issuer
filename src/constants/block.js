3c255c87e149a40d32096112d02de97b7d93c8ee
Nitzan Nashi <nitzan.nashi@zattoo.com>
2022-01-11 16:04:44 +0100

    FIX

--- src/constants/block.js
+++ src/constants/block.js
@@ -1,9 +1,7 @@
-const SPACE = '\n\n';
 const TICKETS_BLOCK_START = '<!-- tickets start -->';
 const TICKETS_BLOCK_END = '<!-- tickets end -->';
 
 module.exports = {
-    SPACE,
     TICKETS_BLOCK_END,
     TICKETS_BLOCK_START,
 }
