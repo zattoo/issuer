50d345ab463c35e2d449388c0cc50c5ffe54481a
Michele Bonazza <michele.bonazza@proton.me>
2025-03-21 09:52:17 +0100

    Add double quotes around expected format and error

--- src/constants/errors.js
+++ src/constants/errors.js
@@ -1,4 +1,4 @@
-const EXPECTED_PATTERN = 'Expected pattern is CODE-XXX [, CODE-XXX]: Title';
+const EXPECTED_PATTERN = 'Expected pattern is "CODE-XXX [, CODE-XXX]: Title"';
 const FORMAT_ERROR = `Format is invalid. ${EXPECTED_PATTERN}`;
 const REFERENCE_ERROR = `Issuer reference is missing. ${EXPECTED_PATTERN}`;
 const SPACE_ERROR = `Title should have space after the separator. ${EXPECTED_PATTERN}`;
@@ -8,5 +8,5 @@ module.exports = {
     FORMAT_ERROR,
     REFERENCE_ERROR,
     SPACE_ERROR,
-    UPPERCASE_ERROR
+    UPPERCASE_ERROR,
 };
--- src/utils.js
+++ src/utils.js
@@ -24,8 +24,10 @@ const getIssuerFromTitle = (title) => {
         return {error: errors.REFERENCE_ERROR};
     }
 
-    if(tickets.some((ticket) => !codeRegex.test(ticket))) {
-        return {error: errors.FORMAT_ERROR};
+    for (const ticket of tickets) {
+        if (!codeRegex.test(ticket)) {
+            return { error: `${errors.FORMAT_ERROR}. Found: "${ticket}"` };
+        }
     }
 
     if(titleString[0] !== ' ') {
